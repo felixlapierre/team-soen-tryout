9a9450e1aa6a4bd862b54bf5565922b657e90be7
Felix <felix.lapierre1@gmail.com>
2018-10-30 18:44:55 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+//Import modules
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+//Create server
+var app = express();
+var server = http.Server(app);
+
+//Network configuration
+app.set('port', 5000);
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//Start the server
+server.listen(5000, function() {
+    console.log('Starting server on port 5000');
+});
