275ee604669c37a0606bf98c18ccd3a4c57a5f71
Thomas Gauvin <t_gauvin@encs.concordia.ca>
2018-10-30 19:55:30 -0400

    mongoose and other dependencies:

--- models/bankStatement.js
+++ models/bankStatement.js
@@ -1,3 +1,17 @@
 var mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+let BankStatement = new Schema({
+    username:{
+        type: String
+    },
+    password:{
+        type: String
+    },
+    balance: {
+        type: integer
+    }
+});
+
+export default mongoose.model('BankStatement', BankStatement);
--- server.js
+++ server.js
@@ -3,13 +3,24 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
 //Create server
 var app = express();
+const router = express.Router();
 var server = http.Server(app);
 
 //Network configuration
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
+app.use(bodyParser.json());
+
+mongoose.connect(['']);
+app.use('/', router);
+
+const connection = mongoose.connection;
+
 
 app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, 'index.html'));
