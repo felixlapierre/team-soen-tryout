23feef88d7322004334258a913af1e7c03018fa2
arejayelle <35614138+arejayelle@users.noreply.github.com>
2018-10-30 20:16:54 -0400

    refined socket.emit

--- static/login.js
+++ static/login.js
@@ -5,14 +5,13 @@ document.addEventListener('onlick', /* some function*/ , false);
 socket.emit('new');
 
 function login(){
-    var user = {
+    console.log("Trying to log in")
+    socket.emit('login',{
         username = document.getElementById("userName").value,
         password = document.getElementById("password").value
-    }
-
-    socket.emit('login', user);
+    } );
 }
 
 function gotoFox(){
-    window.location.replace("https://cdn.shopify.com/s/files/1/3006/2506/products/cute-fox-1-flawed_580x.jpg?v=1518387559")
+    window.location.replace("https://cdn.shopify.com/s/files/1/3006/2506/products/cute-fox-1-flawed_580x.jpg?v=1518387559");
 }
